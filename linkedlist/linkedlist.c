d5a6aaa527e2ea03748428d8d3ac2bbd31b33965
arbobeck <arbobeck@gmail.com>
2023-09-06 16:26:36 +0200

    added C file

---
+++ linkedlist/linkedlist.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+struct Node {
+  int data;
+  struct Node* next;
+}
+
+int main(){
+  struct Node* head = NULL;
+  struct Node* current = NULL;
+
+  for (int i=10; i<=50; i+=10){
+    struct Node* newNode = (struct Node*)malloc(sizeof(struct Node));
+    newNode->data= i;
+    newNode->next = NULL;
+
+    if (head == NULL){
+      head = newNode;
+      current = newNode;
+    } else {
+      current->next = newNode;
+      current = newNode;
+    }
+  }
+}
